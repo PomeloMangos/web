fe00cef621f99363a346d8aa2bf1c78424d823d5
Yuko Zheng <hi@yuko.me>
2019-09-28 17:44:17 +0800

    Enable more stone functions

---
+++ src/Pomelo.WoW.Web/Models/RewardClaim.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pomelo.WoW.Web.Models
+{
+    public class RewardClaim : ModelBase
+    {
+        [Column("character_id")]
+        public uint CharacterId { get; set; }
+
+        [Column("reward_id")]
+        public uint RewardId { get; set; }
+    }
+}
